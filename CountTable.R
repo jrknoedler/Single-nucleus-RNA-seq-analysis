d55e9a4d4b425654d88dce4c70c46fcd61180be1
jrknoedler <jrknoedler@gmail.com>
2025-10-29 16:04:03 -0700

    cleaned up extra files

--- CountTable.R
+++
@@ -1,7 +0,0 @@
-library(Seurat)
-
-mySeurat <- readRDS("Seurat/VMH_IndependentAnalysis/VMHMerged_filtered3_Paperanalysis_20pcs.rds")
-
-mySeurat <- NormalizeData(mySeurat)
-data <- mySeurat@assays[["RNA"]]@counts
-write.table(data, file="ARACHNE/VMH/VMH_Matrixtest.txt")
